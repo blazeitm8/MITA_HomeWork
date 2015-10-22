9468ca087d34de1fde1718410704cf796512798a
blazeitm8 <mello462@gmail.com>
2015-10-23 04:09:51 +0700

    HomeWork-1
    12312123

---
+++ MITA_HomeWork-1/Model/Cat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        CatColor Color = new CatColor();
+
+        public string CurrentColor; 
+        private string _age;
+        private string _name;
+        public int Health = 5;
+
+        public string Name 
+        { 
+            get 
+            {
+                return _name;
+            } 
+            set 
+            {
+                if(string.IsNullOrEmpty(_name))
+                {
+                    _name=value;
+                }
+                else
+                {
+                    Console.WriteLine("Вы уже задали имя, повторно нельзя!");
+                }
+            }
+                
+        }
+
+        public string Age 
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(_age))
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Вы уже задали возраст при покупке, повторно нельзя!");
+                }
+            }
+        }
+        
+        public void Feed()
+        {
+            Health++;
+            CurrentColor = Color.HealthyColor;
+        }
+
+        public void Punish()
+        {
+            Health--;
+            CurrentColor = Color.SickColor;
+        }
+    }
+}
---
+++ MITA_HomeWork-1/Model/CatColor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CatColor
+    {
+        private string _sickcolor;
+        private string _healthycolor;
+
+        public string HealthyColor { get; set; }
+        public string SickColor { get; set; }
+    }
+}
