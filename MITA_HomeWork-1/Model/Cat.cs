e5ac4458e6c343dc6ae3931e24590d0b82ee9c8f
blazeitm8 <mello462@gmail.com>
2015-11-09 01:26:48 +0700

    Домашка-1
    Переделал.

---
+++ MITA_HomeWork-1/Model/Cat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        private string _name;
+
+        private string _currentcolor;
+
+        public CatColor Color { get; set; }
+
+        public int Health = 5;
+
+        public Cat(CatColor color, string age)
+        {
+            Color = color;
+            Age = age;
+        }
+
+        public string CurrentColor
+        {
+            get
+            {
+                 return Health < 5 ? Color.SickColor : Color.HealthyColor;
+            }
+        } 
+
+        public string Name 
+        { 
+            get 
+            {
+                return _name;
+            } 
+            set 
+            {
+                if(string.IsNullOrEmpty(_name))
+                {
+                    _name=value;
+                }
+                else
+                {
+                    Console.WriteLine("Вы уже задали имя, повторно нельзя!");
+                }
+            }    
+        }
+
+        public string Age { get; }
+        
+
+        public void Feed()
+        {
+            Health++;
+        }
+
+        public void Punish()
+        {
+            Health--;
+        }
+    }
+}
---
+++ MITA_HomeWork-1/View/Program.cs
@@ -0,0 +1,79 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            var color = new CatColor();
+
+            string age;
+            
+            Console.Write("Покупка питомца. \nВведите возраст: ");
+            age = Console.ReadLine();
+
+            var cat = new Cat(color, age);
+
+            Console.Write("Задайте цвет для здоровой кошки: ");
+            color.HealthyColor = Console.ReadLine();
+
+            Console.Write("Задайте цвет для больной кошки: ");
+            color.SickColor = Console.ReadLine();
+
+            int i;
+            do
+            {
+                Console.Write("Имя: " + cat.Name);
+                Console.Write("\nВозраст: " + cat.Age);
+                Console.Write("\nЦвет: " + cat.CurrentColor);
+
+                Console.Write("\nМеню:\n1) Задать имя \n2) Ударить! \n3) Покормить \n4) Выйти \n\nВаше решение: ");
+                i = int.Parse(Console.ReadLine());
+                switch (i)
+                {
+                    case 1:
+                        Console.Write("Задать имя: ");
+                        cat.Name = Console.ReadLine();
+                        Console.WriteLine("Имя: " + cat.Name);
+                        break;
+                    case 2:
+                        if (cat.Age == null)
+                        {
+                            Console.WriteLine("Сначала введите возраст!");
+                            break;
+                        }
+                        cat.Punish();
+                        Console.WriteLine("Вы ударили ее и теперь здоровье кошки равно " + cat.Health);
+                        Console.WriteLine("Цвет изменился на " + (cat.CurrentColor));
+                        break;
+                    case 3:
+                        if (cat.Age == null)
+                        {
+                            Console.WriteLine("Сначала введите возраст!");
+                            break;
+                        }
+                        cat.Feed();
+                        Console.WriteLine("Вы покормили ее и теперь здоровье кошки равно " + cat.Health);
+                        Console.WriteLine("Цвет изменился на " + (cat.CurrentColor));
+                        break;
+                    case 4:
+                        Console.WriteLine("Вы решили выйти");
+                        break;
+                    default:
+                        Console.WriteLine("Вы что-то другое нажали...");
+                        break;
+                }
+                Console.Write("\n\n");
+              
+            }
+            while (i != 4);
+        }
+    }
+}
